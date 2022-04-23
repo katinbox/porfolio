bd57fcc8eccfe9b11603b149bb1eae90bebb96b5
Thien Khanh <tranquanki28@gmail.com>
2022-04-23 23:41:20 +0700

    style: contact section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 (() => {
-  const filterContainer = document.querySelector(".porfolio-filter")
-  const porfolioItemContainer = document.querySelector(".porfolio-items")
-  const porfolioItems = document.querySelectorAll(".porfolio-item")
+  const filterContainer = document.querySelector(".porfolio-filter"),
+    porfolioItems = document.querySelectorAll(".porfolio-item")
 
   filterContainer.addEventListener("click", (e) => {
     if (e.target.classList.contains('filter-item') && !e.target.classList.contains('active')) {
@@ -9,7 +8,7 @@
       e.target.classList.add("active", "outer-shadow")
       const target = e.target.getAttribute('data-target')
       porfolioItems.forEach((item) => {
-        if (target  === item.getAttribute('data-category') || target === 'all') {
+        if (target === item.getAttribute('data-category') || target === 'all') {
           item.classList.remove("hide")
           item.classList.add("show")
         }
@@ -22,3 +21,38 @@
   })
 }
 )();
+
+
+(() => {
+  const sliderContainer = document.querySelector('.testi-container'),
+    sliders = sliderContainer.querySelectorAll('.testi-item'),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector('.testti-slider-nav .prev'),
+    nextBtn = document.querySelector('.testti-slider-nav .next')
+
+  let slideIndex = 0;
+  sliders.forEach((slider) => {
+    slider.style.width = slideWidth + 'px';
+  })
+  sliderContainer.style.width = slideWidth * sliders.length + 'px';
+  nextBtn.addEventListener('click', () => {
+    if (slideIndex === sliders.length - 1) {
+      slideIndex = 0;
+    }
+    else {
+      slideIndex++;
+    }
+    slider()
+  })
+  prevBtn.addEventListener('click', () => {
+    if (slideIndex === 0) {
+      slideIndex = sliders.length - 1;
+    }
+    else {
+      slideIndex--;
+    }
+    slider()
+  })
+
+  const slider = () => sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + 'px'
+})();
